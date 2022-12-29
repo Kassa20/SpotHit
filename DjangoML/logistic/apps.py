414cbb1742463d44c597ed57547b0e8a9e001dd1
Kassa20 <109251004+Kassa20@users.noreply.github.com>
2022-12-29 16:01:12 -0600

    Created Basic Django Skeleton

---
+++ DjangoML/logistic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LogisticregressionConfig(AppConfig):
+    name = 'LogisticRegression'
---
+++ DjangoML/logistic/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "logistic/index.html")
+
+def user(request):
+    username = request.GET['username']
+    return render(request, 'user.html', {
+        'name': username
+    })
